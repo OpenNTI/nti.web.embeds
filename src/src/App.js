5d2bb36679a54222bfd7be00888311d4f124ef50
slidingsteven <steven.tuckerba@ou.edu>
2020-01-21 18:44:29 -0600

    Adding the card with API interaction

--- src/src/App.js
+++ src/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import Card from './components/Card'
-import Frame from 'react-frame-component';
+import Card from './components/Card';
+import Frame from './components/Frame';
 
+const BASE_URL = 'https://alpha.nextthought.com'
+const REQ_URL = BASE_URL+'/dataserver2/++etc++hostsites/alpha.nextthought.com/++etc++site/Courses/DefaultAPICreated/OUCS-1/CourseCatalogEntry'
+const IMAGE_NAME = 'contentpackage-landing-232x170.png'
+//const REQ_IMG_URL = BASE_URL+'content/sites/alpha.nextthought.com/Courses/DefaultAPICreated/OUCS-1/presentation-assets/webapp/v1/contentpackage-landing-232x170.png'
 
-const REQ_URL = 'https://alpha.nextthought.com/dataserver2/++etc++hostsites/alpha.nextthought.com/++etc++site/Courses/DefaultAPICreated/OUCS-1/CourseCatalogEntry'
 class App extends React.Component {
 
   constructor(props){
@@ -16,19 +19,22 @@ class App extends React.Component {
   componentDidMount = () => {
     fetch(REQ_URL, {
       headers: {
-        'X-Requested-With': 'XMLHTTPRequest'
+        'X-Requested-With': 'XMLHTTPRequest',
+        'User-Agent' : 'NextThought OUCS Capstone 1920'
       }
     }).then(response => response.json().then(courseCatalog => this.setState({courses: [courseCatalog]})) )
+
   }
+
 
   render(){
     return (
-      <Frame style={{width: '100%', border: 'none'}}>
+      //<Frame style={{width: '100%', border: 'none'}}>
         <div className="App">
-          {this.state.courses.map(course => <Card title={course.title}
-          description={course.RichDescription} />)}
+          {this.state.courses.map(course => <Card title={course.ProviderDisplayName}
+          description={course.DCTitle} image={BASE_URL+course.PlatformPresentationResources[0].href + IMAGE_NAME}  />)}
         </div>
-      </Frame>
+      //</Frame>
     )
   }
 }
--- src/src/components/Card.js
+++ src/src/components/Card.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { checkPropTypes } from 'prop-types';
 
+
+function toggleMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
 const Card = (props) => (
-    <div style={{
-      backgroundColor: 'white'
-    }}> 
-      <h4>{props.title}</h4> 
-      <div>
-        {props.description} 
-      </div> 
-    </div>
+  //<iframe title={props.title} id={props.title} frameBorder="0" scrolling="no" width ="250" height="350" src="iframetest.html"></iframe>
+  <a href="#"><div scrolling="no" style={{
+    backgroundColor: 'white',
+    height:"250px",
+    width:"225px"
+
+  }}> 
+    <img src={props.image} />
+
+    <p style={{backgroundColor:'black', color:'white'}}>{props.title}</p> 
+    <div>
+      {props.description} 
+    </div> 
+
+  </div></a>
+  
+
   )
 
 export default Card
