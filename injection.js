c8df1caf0209e0a6b47a02380d13d8f19c3fef54
slidingsteven <steven.tuckerba@ou.edu>
2020-02-26 12:07:21 -0600

    not sure what I'm committing

--- injection.js
+++ injection.js
@@ -9,5 +9,6 @@ const thisScript = document.getElementById('capstone')
 const darkMode = thisScript.getAttribute('darkMode') || 'false'
 const flowDirection = thisScript.getAttribute('direction')
 const courseID = thisScript.getAttribute('courseId') || ''
+//iFrame.style = "overflow:hidden"
 iFrame.src = `http://127.0.0.1:3000/build/index.html?darkmode=${darkMode}&direction=${flowDirection}&courseID=${courseID}`
 thisScript.parentNode.insertBefore(iFrame, thisScript.nextSibling)
