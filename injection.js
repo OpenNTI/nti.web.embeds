cd283349f592b7eb4e1dac7e34736b93c9e6ca82
Imra Maududi Qureshi <imra.maududi.qureshi-1@ou.edu>
2020-03-03 23:31:15 -0600

    Removed scrollbars from course card

--- injection.js
+++ injection.js
@@ -9,5 +9,5 @@ const thisScript = document.getElementById('capstone')
 const darkMode = thisScript.getAttribute('darkMode') || 'false'
 const flowDirection = thisScript.getAttribute('direction')
 const courseID = thisScript.getAttribute('courseId') || ''
-iFrame.src = `http://127.0.0.1:5500/build/index.html?darkmode=${darkMode}&direction=${flowDirection}&courseID=${courseID}`
+iFrame.src = `http://127.0.0.1:3000/build/index.html?darkmode=${darkMode}&direction=${flowDirection}&courseID=${courseID}`
 thisScript.parentNode.insertBefore(iFrame, thisScript.nextSibling)
